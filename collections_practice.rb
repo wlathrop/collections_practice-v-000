698cdeba6c48385443f0d71a8b4053a3b315923c
William Lathrop <wlathrop10@gmail.com>
2018-08-31 05:11:43 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,3 +20,35 @@ def swap_elements_from_to(array, index, destination_index)
   return array
 end
 
+def reverse_array(integers)
+  new_array = integers.reverse
+  new_array
+end
+
+def kesha_maker(array)
+  kesha = []
+  array.each do |word|
+    word_array = word.split ""
+    word_array[2] = "$"
+    kesha << word_array.join
+  end
+  kesha
+end
+
+def find_a(array)
+  array.select{|string| string.start_with?("a")}
+end
+
+def sum_array(integers)
+  integers.inject{|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+end
