0d6be6efebb7d6ace397b7ea7bb73d0863b1212a
William Lathrop <wlathrop10@gmail.com>
2018-08-31 05:10:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,3 +6,6 @@ def sort_array_desc(integers)
   integers.sort {|first_num, second_num| second_num <=> first_num}
 end
 
+def sort_array_char_count(strings)
+  strings.sort {|left, right| left.length <=> right.length}
+end
