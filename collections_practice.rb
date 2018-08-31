9b4853f4292fb6d03acddbd21a7c0349fa206de3
William Lathrop <wlathrop10@gmail.com>
2018-08-31 05:11:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,3 +15,8 @@ def swap_elements(strings)
   return strings
 end
 
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  return array
+end
+
